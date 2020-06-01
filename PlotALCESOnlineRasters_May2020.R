238a993cb8cf3294d95585fde7dcce1e1fdf9b0f
Lionel Leston <leston@ualberta.ca>
2020-06-01 10:35:01 -0500

    Species maps for Black-backed Woodpecker, Black-throated Green Warbler, Brown Creeper, Cape May Warbler, Palm Warbler, Rusty Blackbird
    Species maps for Black-backed Woodpecker, Black-throated Green Warbler, Brown Creeper, Cape May Warbler, Palm Warbler, Rusty Blackbird

--- PlotALCESOnlineRasters_May2020.R
+++ PlotALCESOnlineRasters_May2020.R
@@ -21,156 +21,188 @@ my.theme <- theme_classic() +
 
 DIR<-getwd()
 
-#str_name<-'BRCR_BAU_Year0.tif' 
-r0.brcr.bau=raster(paste0(DIR,"/prediction rasters/","BRCR_BAU_Year0.tif"))
-r50.brcr.bau=raster(paste0(DIR,"/prediction rasters/","BRCR_BAU_Year50.tif"))
+names<-c("CapeMayWarbler",
+         "PalmWarbler",
+         "RustyBlackbird") 
 
-r0.brcr.0fire=raster(paste0(DIR,"/prediction rasters/","BRCR_NoFire_Year0.tif"))
-r50.brcr.0fire=raster(paste0(DIR,"/prediction rasters/","BRCR_NoFire_Year50.tif"))
+miat=c(-4,-3.5,-3,-2.5,-2,-1.5,-1,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4)
+#this vector will be used to create a common set of values for the 
+#colour legend in all four plots; otherwise, legends would be 
+#independent and may give deceptive results when comparing scenarios
 
-r0.brcr.2xfire=raster(paste0(DIR,"/prediction rasters/","BRCR_IncreasedFire_Year0.tif"))
-r50.brcr.2xfire=raster(paste0(DIR,"/prediction rasters/","BRCR_IncreasedFire_Year50.tif"))
+for (SPP in names){
+  r0.bau=raster(paste0(DIR,"/prediction rasters/",SPP,"_BAU_Year0.tif"))
+  r50.bau=raster(paste0(DIR,"/prediction rasters/",SPP,"_BAU_Year50.tif"))
+  
+  r0.0fire=raster(paste0(DIR,"/prediction rasters/",SPP,"_NoFire_Year0.tif"))
+  r50.0fire=raster(paste0(DIR,"/prediction rasters/",SPP,"_NoFire_Year50.tif"))
+  
+  r0.0energy=raster(paste0(DIR,"/prediction rasters/",SPP,"_NoEnergy_Year0.tif"))
+  r50.0energy=raster(paste0(DIR,"/prediction rasters/",SPP,"_NoEnergy_Year50.tif"))
+  
+  r0.2xfire=raster(paste0(DIR,"/prediction rasters/",SPP,"_IncreasedFire_Year0.tif"))
+  r50.2xfire=raster(paste0(DIR,"/prediction rasters/",SPP,"_IncreasedFire_Year50.tif"))
+  
 
-#Basic rasterVis plot
-levelplot(r0.brcr.bau, margin=F)
-
-#Year 0 (BAU scenario)
-r0.brcr.bau_df <- as.data.frame(r0.brcr.bau, xy = TRUE)
-str(r0.brcr.bau_df)
-r0.brcr.bau_df$meandens = ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<0.125,0,
-                                     ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<0.375,0.25,
-                                            ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<0.625,0.5,
-                                                   ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<0.75,0.625,
-                                                          ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<0.875,0.75,
-                                                                 ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<1,0.875,
-                                                                        ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<1.5,1,
-                                                                               ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<2.5,2,
-                                                                                      ifelse(r0.brcr.bau_df$BRCR_BAU_Year0<3.5,3,4)))))))))
-
-CRS.new<-crs(r0.brcr.bau)
-r0.brcr.bau<-rasterFromXYZ(r0.brcr.bau_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
-#get rid of box, axes, axis values
-levelplot(r0.brcr.bau, margin=F, par.settings = list(axis.line = list(col = "transparent"), 
-                                               strip.background = list(col = 'transparent'), 
-                                               strip.border = list(col = 'transparent')), scales = list(col = "transparent"))
-
-
-
-#change raster colours
-mapTheme<-rasterTheme(region = brewer.pal(8, "Greens"))
-
-levelplot(r0.brcr.bau, margin=F, 
-          par.settings = list(axis.line = list(col = "transparent"), 
-                                             strip.background = list(col = 'transparent'), 
-                                             strip.border = list(col = 'transparent')), scales = list(col = "transparent"))
-
-levelplot(r0.brcr.bau, margin=F, par.settings=BuRdTheme())
-
-#Year 50 (BAU scenario)
-r50.brcr.bau_df <- as.data.frame(r50.brcr.bau, xy = TRUE)
-str(r50.brcr.bau_df)
-r50.brcr.bau_df$meandens = ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<0.125,0,
-                                 ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<0.375,0.25,
-                                        ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<0.625,0.5,
-                                               ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<0.75,0.625,
-                                                      ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<0.875,0.75,
-                                                             ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<1,0.875,
-                                                                    ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<1.5,1,
-                                                                           ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<2.5,2,
-                                                                                  ifelse(r50.brcr.bau_df$BRCR_BAU_Year50<3.5,3,4)))))))))
-
-CRS.new<-crs(r50.brcr.bau)
-r50.brcr.bau<-rasterFromXYZ(r50.brcr.bau_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
-
-#Year 50 - Year 0 (BAU scenario)
-bau<-levelplot(r50.brcr.bau-r0.brcr.bau, margin=F,
-               par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
-                                   strip.background = list(col = 'transparent'), 
-                                   strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
-               main=list(label='Current Fire: Year 50 - Year 0',cex=0.7))
-
-#Plot change over time
-#Year 0
-r0.brcr.2xfire_df <- as.data.frame(r0.brcr.2xfire, xy = TRUE)
-str(r0.brcr.2xfire_df)
-r0.brcr.2xfire_df$meandens = ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<0.125,0,
-                                 ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<0.375,0.25,
-                                        ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<0.625,0.5,
-                                               ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<0.75,0.625,
-                                                      ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<0.875,0.75,
-                                                             ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<1,0.875,
-                                                                    ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<1.5,1,
-                                                                           ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<2.5,2,
-                                                                                  ifelse(r0.brcr.2xfire_df$BRCR_IncreasedFire_Year0<3.5,3,4)))))))))
-
-CRS.new<-crs(r0.brcr.2xfire)
-r0.brcr.2xfire<-rasterFromXYZ(r0.brcr.2xfire_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
-
-#Year 50
-r50.brcr.2xfire_df <- as.data.frame(r50.brcr.2xfire, xy = TRUE)
-str(r50.brcr.2xfire_df)
-r50.brcr.2xfire_df$meandens = ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<0.125,0,
-                                 ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<0.375,0.25,
-                                        ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<0.625,0.5,
-                                               ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<0.75,0.625,
-                                                      ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<0.875,0.75,
-                                                             ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<1,0.875,
-                                                                    ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<1.5,1,
-                                                                           ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<2.5,2,
-                                                                                  ifelse(r50.brcr.2xfire_df$BRCR_IncreasedFire_Year50<3.5,3,4)))))))))
-
-CRS.new<-crs(r50.brcr.2xfire)
-r50.brcr.2xfire<-rasterFromXYZ(r50.brcr.2xfire_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
-
-#Year 50-0
-inc.fire<-levelplot(r50.brcr.2xfire-r0.brcr.2xfire, margin=F,
-                    par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
-                                               strip.background = list(col = 'transparent'), 
-                                               strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
-                    main=list(label='Increased Fire: Year 50 - Year 0',cex=0.7))
-
-
-
-#Year 50 Increased Fire -Year 50 BAU
-inc.fire.minus.bau<-levelplot(r50.brcr.2xfire-r50.brcr.bau, margin=F,
-                    par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
-                                               strip.background = list(col = 'transparent'), 
-                                               strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
-                    main=list(label='Year 50: Increased Fire - BAU',cex=0.7))
-
-
-
-lattice.options(
-  layout.heights=list(bottom.padding=list(x=0), top.padding=list(x=0)),
-  layout.widths=list(left.padding=list(x=0), right.padding=list(x=0.5))
-)
-
-# bau$par.settings$layout.heights[
-#   c( 'bottom.padding',
-#      'top.padding',
-#      'key.sub.padding',
-#      'axis.xlab.padding',
-#      'key.axis.padding',
-#      'main.key.padding') ] <- -1
-# bau$aspect.fill <- TRUE
-# 
-# inc.fire$par.settings$layout.heights[
-#   c( 'bottom.padding',
-#      'top.padding',
-#      'key.sub.padding',
-#      'axis.xlab.padding',
-#      'key.axis.padding',
-#      'main.key.padding') ] <- -1
-# inc.fire$aspect.fill <- TRUE
-
-tg<-textGrob("Change in Brown Creeper Density",gp=gpar(fontsize=10,font=1))
-margin <- unit(0.5, "line")
-grided<-grid.arrange(bau, inc.fire, bau, ncol=2)
-
-tiff('ALCESOnline_BrownCreeperDensityMap.tiff', units="in", width=15, height=15, res=300)
-
-grid.arrange(tg, grided,
-             heights = unit.c(grobHeight(tg) + 1.2*margin, 
-                          unit(1,"null")))
-dev.off()
+  #Plot change over time for BAU Scenario
+  #Year 0 (BAU Scenario)
+  r0.bau_df <- as.data.frame(r0.bau, xy = TRUE)
+  str(r0.bau_df)
+  r0.bau_df$value<-r0.bau_df[,c(paste0(SPP,"_BAU_Year0"))]
+  r0.bau_df$meandens = ifelse(is.na(r0.bau_df$value),"NA",
+                              ifelse(r0.bau_df$value>4,4,r0.bau_df$value))
+  
+  CRS.new<-crs(r0.bau)
+  r0.bau<-rasterFromXYZ(r0.bau_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  #get rid of box, axes, axis values
+  levelplot(r0.bau, margin=F, par.settings = list(axis.line = list(col = "transparent"), 
+                                                  strip.background = list(col = 'transparent'), 
+                                                  strip.border = list(col = 'transparent')), scales = list(col = "transparent"))
+  
+  
+  
+  #change raster colours
+  mapTheme<-rasterTheme(region = brewer.pal(8, "Greens"))
+  
+  levelplot(r0.bau, margin=F, 
+            par.settings = list(axis.line = list(col = "transparent"), 
+                                strip.background = list(col = 'transparent'), 
+                                strip.border = list(col = 'transparent')), scales = list(col = "transparent"))
+  
+  levelplot(r0.bau, margin=F, par.settings=BuRdTheme())
+  
+  #Year 50 (BAU scenario)
+  r50.bau_df <- as.data.frame(r50.bau, xy = TRUE)
+  str(r50.bau_df)
+  r50.bau_df$value<-r50.bau_df[,c(paste0(SPP,"_BAU_Year50"))]
+  r50.bau_df$meandens = ifelse(is.na(r50.bau_df$value),"NA",
+                               ifelse(r50.bau_df$value>4,4,r50.bau_df$value))
+  
+  
+  CRS.new<-crs(r50.bau)
+  r50.bau<-rasterFromXYZ(r50.bau_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50 - Year 0 (BAU scenario)
+  bau<-levelplot(r50.bau-r0.bau, margin=F,
+                 par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
+                                            strip.background = list(col = 'transparent'), 
+                                            strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
+                 main=list(label='Current Fire: Year 50 - Year 0',cex=1),at=miat)
+  
+  #Plot change over time for Increased Fire Scenario
+  #Year 0 (Increased Fire Scenario)
+  r0.2xfire_df <- as.data.frame(r0.2xfire, xy = TRUE)
+  str(r0.2xfire_df)
+  r0.2xfire_df$value<-r0.2xfire_df[,c(paste0(SPP,"_IncreasedFire_Year0"))]
+  r0.2xfire_df$meandens = ifelse(is.na(r0.2xfire_df$value),"NA",
+                                 ifelse(r0.2xfire_df$value>4,4,r0.2xfire_df$value))
+  
+  
+  CRS.new<-crs(r0.2xfire)
+  r0.2xfire<-rasterFromXYZ(r0.2xfire_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50 (Increased Fire Scenario)
+  r50.2xfire_df <- as.data.frame(r50.2xfire, xy = TRUE)
+  str(r50.2xfire_df)
+  r50.2xfire_df$value<-r50.2xfire_df[,c(paste0(SPP,"_IncreasedFire_Year50"))]
+  r50.2xfire_df$meandens = ifelse(is.na(r50.2xfire_df$value),"NA",
+                                  ifelse(r50.2xfire_df$value>4,4,r50.2xfire_df$value))
+  
+  
+  CRS.new<-crs(r50.2xfire)
+  r50.2xfire<-rasterFromXYZ(r50.2xfire_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50-0 (Increased Fire Scenario)
+  inc.fire<-levelplot(r50.2xfire-r0.2xfire, margin=F,
+                      par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
+                                                 strip.background = list(col = 'transparent'), 
+                                                 strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
+                      main=list(label='Increased Fire: Year 50 - Year 0',cex=1),at=miat)
+  
+  
+  
+  #Plot change over time for No Fire Scenario
+  #Year 0 (No Fire Scenario)
+  r0.0fire_df <- as.data.frame(r0.0fire, xy = TRUE)
+  str(r0.0fire_df)
+  r0.0fire_df$value<-r0.0fire_df[,c(paste0(SPP,"_NoFire_Year0"))]
+  r0.0fire_df$meandens = ifelse(is.na(r0.0fire_df$value),"NA",
+                                ifelse(r0.0fire_df$value>4,4,r0.0fire_df$value))
+  
+  
+  CRS.new<-crs(r0.0fire)
+  r0.0fire<-rasterFromXYZ(r0.0fire_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50 (No Fire Scenario)
+  r50.0fire_df <- as.data.frame(r50.0fire, xy = TRUE)
+  str(r50.0fire_df)
+  r50.0fire_df$value<-r50.0fire_df[,c(paste0(SPP,"_NoFire_Year50"))]
+  r50.0fire_df$meandens = ifelse(is.na(r50.0fire_df$value),"NA",
+                                 ifelse(r50.0fire_df$value>4,4,r50.0fire_df$value))
+  
+  
+  CRS.new<-crs(r50.0fire)
+  r50.0fire<-rasterFromXYZ(r50.0fire_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50-0 (No Fire Scenario)
+  no.fire<-levelplot(r50.0fire-r0.0fire, margin=F,
+                     par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
+                                                strip.background = list(col = 'transparent'), 
+                                                strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
+                     main=list(label='No Fire: Year 50 - Year 0',cex=1),at=miat)
+  
+  
+  
+  
+  #Plot change over time for No Energy Scenario
+  #Year 0 (No Energy Scenario)
+  r0.0energy_df <- as.data.frame(r0.0energy, xy = TRUE)
+  str(r0.0energy_df)
+  r0.0energy_df$value<-r0.0energy_df[,c(paste0(SPP,"_NoEnergy_Year0"))]
+  r0.0energy_df$meandens = ifelse(is.na(r0.0energy_df$value),"NA",
+                                  ifelse(r0.0energy_df$value>4,4,r0.0energy_df$value))
+  
+  
+  CRS.new<-crs(r0.0energy)
+  r0.0energy<-rasterFromXYZ(r0.0energy_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50 (No Energy Scenario)
+  r50.0energy_df <- as.data.frame(r50.0energy, xy = TRUE)
+  str(r50.0energy_df)
+  r50.0energy_df$value<-r50.0energy_df[,c(paste0(SPP,"_NoEnergy_Year50"))]
+  r50.0energy_df$meandens = ifelse(is.na(r50.0energy_df$value),"NA",
+                                   ifelse(r50.0energy_df$value>4,4,r50.0energy_df$value))
+  
+  
+  CRS.new<-crs(r50.0energy)
+  r50.0energy<-rasterFromXYZ(r50.0energy_df[,c("x","y","meandens")],res=c(200,200),crs=CRS.new,digits=2)
+  
+  #Year 50-0 (No Energy Scenario)
+  no.energy<-levelplot(r50.0energy-r0.0energy, margin=F,
+                       par.settings = rasterTheme(region=magma(10), axis.line = list(col = "transparent"), 
+                                                  strip.background = list(col = 'transparent'), 
+                                                  strip.border = list(col = 'transparent')), scales = list(col = "transparent"),
+                       main=list(label='No Energy: Year 50 - Year 0',cex=1),at=miat)
+  
+  
+  
+  
+  lattice.options(
+    layout.heights=list(bottom.padding=list(x=0), top.padding=list(x=0)),
+    layout.widths=list(left.padding=list(x=0), right.padding=list(x=0.5))
+  )
+  
+  
+  tg<-textGrob(paste0("Change in ",SPP," Density"),gp=gpar(fontsize=15,font=1))
+  margin <- unit(0.5, "line")
+  grided<-grid.arrange(bau, inc.fire, no.fire, no.energy, nrow=2, ncol=2)
+  
+  tiff(paste0("species maps/ALCESOnline_",SPP,"DensityMap.tiff"), units="in", width=15, height=15, res=300)
+  
+  grid.arrange(tg, grided,
+               heights = unit.c(grobHeight(tg) + 1.2*margin, 
+                                unit(1,"null")))
+  dev.off()
+  print(paste0("Map printed for ",SPP))
+}
 
