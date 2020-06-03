ec6112c842cb900d1908c056ba2204382e87317c
Lionel Leston <leston@ualberta.ca>
2020-06-02 23:16:55 -0500

    New indicators
    New TIFF map files for Blackpoll Warbler, Boreal Chickadee, Ovenbird, Pileated Woodpecker, Western Tanager.
    
    Scripts for generating preferred habitat indicators and species indicators for multiple species automatically.

--- PlotALCESOnlineRasters_May2020.R
+++ PlotALCESOnlineRasters_May2020.R
@@ -21,9 +21,8 @@ my.theme <- theme_classic() +
 
 DIR<-getwd()
 
-names<-c("CapeMayWarbler",
-         "PalmWarbler",
-         "RustyBlackbird") 
+names<-c("Ovenbird",
+         "WesternTanager") 
 
 miat=c(-4,-3.5,-3,-2.5,-2,-1.5,-1,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4)
 #this vector will be used to create a common set of values for the 
